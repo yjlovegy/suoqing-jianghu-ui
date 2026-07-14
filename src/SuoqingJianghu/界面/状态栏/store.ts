f3dfdb21666fac0db03ad8e61282c3617de7afa4
yjlovegy <yaojianagy@163.com>
2026-07-14 19:59:47 +0800

    Add src/SuoqingJianghu/界面/状态栏/store.ts

---
+++ src/SuoqingJianghu/界面/状态栏/store.ts
@@ -0,0 +1,7 @@
+import { defineMvuDataStore } from '@util/mvu';
+import { Schema } from '../../schema';
+
+export const useDataStore = defineMvuDataStore(Schema, {
+  type: 'message',
+  message_id: getCurrentMessageId(),
+});
